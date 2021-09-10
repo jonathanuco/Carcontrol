ae0838fa21c35ceb178fdd57cf546579add7c784
jonathanuco <jonathan.miranda5320@soyuco.edu.co>
2021-09-09 20:12:20 -0500

    Proyecto inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { ListarComponent } from './componentes/listar/listar.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'listar', component: ListarComponent},
+  { path: 'formulario', component: FormularioComponent},
+  { path: '**', pathMatch: 'full' , redirectTo: 'home'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
